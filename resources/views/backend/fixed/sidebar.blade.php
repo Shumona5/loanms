195592767ed277794f0bc8f72934bf2d138976cf
Tifoysal <foysal11203004@gmail.com>
2022-12-11 01:26:25 +0600

    update sidebar icon

--- resources/views/backend/fixed/sidebar.blade.php
+++ resources/views/backend/fixed/sidebar.blade.php
@@ -2,7 +2,7 @@
     <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
         <div class="sb-sidenav-menu">
             <div class="nav">
-                
+
                 <a class="nav-link" href="{{route('dashboard')}}">
                     <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                     Dashboard
@@ -11,61 +11,61 @@
 
 
                 <a class="nav-link" href="{{route('admin')}}">
-                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                    <div class="sb-nav-link-icon"><i class="fas fa-user-circle"></i></div>
                     Admin
                 </a>
 
 
                 <a class="nav-link" href="{{route ('bank')}}">
-                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                    <div class="sb-nav-link-icon"><i class="fas fa-home"></i></div>
                     Bank
                 </a>
 
 
                 <a class="nav-link" href="{{route('user')}}">
-                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                    <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>
                     Loan Seeker
                 </a>
 
                 <a class="nav-link" href="{{route('loan')}}">
-                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                    <div class="sb-nav-link-icon"><i class="fas fa-money-bill-alt"></i></div>
                     Loan
                 </a>
 
                 <a class="nav-link" href="{{route('loanapply')}}">
-                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                    <div class="sb-nav-link-icon"><i class="fas fa-list-alt"></i></div>
                     Loan Apply
                 </a>
 
 
                 <a class="nav-link" href="{{route('criteria')}}">
-                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                    Criteria
+                    <div class="sb-nav-link-icon"><i class="fas fa-cogs"></i></div>
+                   Loan Criteria
                 </a>
 
 
-                <a class="nav-link" href="{{route('loancriteria')}}">
-                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-                    Loan Criteria
-                </a>
+{{--                <a class="nav-link" href="{{route('loancriteria')}}">--}}
+{{--                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>--}}
+{{--                    Loan Criteria--}}
+{{--                </a>--}}
 
                 <a class="nav-link" href="{{route('loantype')}}">
-                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                    <div class="sb-nav-link-icon"><i class="fas fa-list-ul"></i></div>
                     Loan Type
                 </a>
 
 
                 <a class="nav-link" href="{{route('report')}}">
-                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                    <div class="sb-nav-link-icon"><i class="fas fa-clipboard"></i></div>
                     Report
                 </a>
 
                 <a class="nav-link" href="{{route('fav')}}">
-                    <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                    <div class="sb-nav-link-icon"><i class="fas fa-heart"></i></div>
                     Favourite List
                 </a>
-                
-                
-            
+
+
+
     </nav>
 </div>
